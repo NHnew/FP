f0e3ee1f69ad5c4010fe1be141db15b38ff8f081
NHnew nabi <nbihuseynov1@gmail.com>
2024-04-03 16:00:11 +0000

    headerAddStyle

--- src/components/Header/Headers.jsx
+++ src/components/Header/Headers.jsx
@@ -7,11 +7,15 @@ import { FiMenu } from "react-icons/fi";
 import { IoMdClose } from "react-icons/io";
 import { GrLanguage } from "react-icons/gr";
 import { useEffect } from 'react';
+import Profile from '../../assets/2Nabi3x4.jpg';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
 
 
 
 
 const Headers = () => {
+
 
     const [menuIcon, setMenuIcon] = useState(true);
     const [menuVisible, setMenuVisible] = useState(false);
@@ -20,6 +24,10 @@ const Headers = () => {
         const body = document.body;
         menuVisible ? body.style.overflow = 'hidden' : body.style.overflow = '';
     }, [menuVisible]);
+
+    function classNames(...classes) {
+        return classes.filter(Boolean).join(' ');
+    }
 
     const handleMenuClick = () => {
         setMenuIcon(prevState => !prevState);
@@ -33,7 +41,7 @@ const Headers = () => {
 
                 <div className="menu flex items-center justify-between">
                     <div onClick={handleMenuClick} className='hamburgerMenu'>
-                        {menuIcon ? <FiMenu /> : <IoMdClose />}
+                        {menuIcon ? <FiMenu className='openHamburgerMenu' /> : <IoMdClose className='closeHamburgerMenu' />}
                     </div>
                     <Link to={'/home'}>
                         <div className='logo'>
@@ -54,7 +62,7 @@ const Headers = () => {
                             <li className='menuText'>Digər</li>
                         </Link>
                         <li className='inputBox w-[150px]'>
-                            <div className='w-full flex items-center bg-white rounded-3xl pl-7 overflow-hidden'>
+                            <div className='subInputBox w-full flex items-center bg-white rounded-3xl pl-7 overflow-hidden'>
                                 <IoSearchSharp className='icon' />
                                 <input className='w-full px-2' type="text" placeholder='Axtarış' />
                             </div>
@@ -63,9 +71,61 @@ const Headers = () => {
                             <GrLanguage />
                         </li>
                     </ul>
-                    <div className='profile text-white w-9 h-9 bg-white rounded-full'>
-
-                    </div>
+                    <Menu as="div" className="relative ml-3">
+                        <div>
+                            <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm">
+                                <span className="absolute -inset-1.5" />
+                                <span className="sr-only">Open user menu</span>
+                                <img
+                                    className="h-8 w-8 rounded-full"
+                                    src={Profile}
+                                    alt=""
+                                />
+                            </Menu.Button>
+                        </div>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-stone-800 py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-fuchsia-700' : '', 'block px-4 py-2 text-sm text-white')}
+                                        >
+                                            Your Profile
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-fuchsia-700' : '', 'block px-4 py-2 text-sm text-white')}
+                                        >
+                                            Settings
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <a
+                                            href="#"
+                                            className={classNames(active ? 'bg-fuchsia-700' : '', 'block px-4 py-2 text-sm text-white')}
+                                        >
+                                            Sign out
+                                        </a>
+                                    )}
+                                </Menu.Item>
+                            </Menu.Items>
+                        </Transition>
+                    </Menu>
                 </div>
             </div>
         </header>
