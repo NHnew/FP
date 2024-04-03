04304b37cd918ba856d5388b2646c537a57438a2
NHnew <157475888+NHnew@users.noreply.github.com>
2024-04-04 02:16:02 +0400

    Update Headers.jsx

--- src/components/Header/Headers.jsx
+++ src/components/Header/Headers.jsx
@@ -61,7 +61,7 @@ const Headers = () => {
                         <Link className='link' to={'/other'}>
                             <li className='menuText'>Digər</li>
                         </Link>
-                        <li className='inputBox w-[150px]'>
+                        <li className='inputBox'>
                             <div className='subInputBox w-full flex items-center bg-white rounded-3xl pl-7 overflow-hidden'>
                                 <IoSearchSharp className='icon' />
                                 <input className='w-full px-2' type="text" placeholder='Axtarış' />
